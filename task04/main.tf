9fa33dcd1f957ddc35fd94e1c04608a45332eda7
Shashwat Swaraj <shashwat_swaraj@epam.com>
2025-03-17 15:56:04 +0530

    done1

--- task04/main.tf
+++ task04/main.tf
@@ -1,24 +1,40 @@
+# Create the resource group
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
   tags     = var.tags
 }
 
+# Artificial delay after resource group creation (helps with Azure propagation delays)
+resource "null_resource" "delay" {
+  depends_on = [azurerm_resource_group.rg]
+  provisioner "local-exec" {
+    command = "sleep 15" # Delay for 15 seconds
+  }
+}
+
+# Create the virtual network
 resource "azurerm_virtual_network" "vnet" {
   name                = var.virtual_network_name
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.0.0.0/16"]
   tags                = var.tags
+
+  depends_on = [null_resource.delay]
 }
 
+# Create the subnet inside the virtual network
 resource "azurerm_subnet" "subnet" {
   name                 = var.subnet_name
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
+
+  depends_on = [azurerm_virtual_network.vnet]
 }
 
+# Create the public IP address
 resource "azurerm_public_ip" "pip" {
   name                = var.public_ip_name
   location            = var.location
@@ -27,20 +43,20 @@ resource "azurerm_public_ip" "pip" {
   domain_name_label   = var.dns_name_label
   tags                = var.tags
 
-  # Explicit dependency added
-  depends_on = [azurerm_resource_group.rg]
+  depends_on = [null_resource.delay]
 }
 
+# Create the network security group
 resource "azurerm_network_security_group" "nsg" {
   name                = var.nsg_name
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
   tags                = var.tags
 
-  # Explicit dependency added
-  depends_on = [azurerm_resource_group.rg]
+  depends_on = [null_resource.delay]
 }
 
+# Create the HTTP rule for the NSG
 resource "azurerm_network_security_rule" "http_rule" {
   name                        = var.http_rule_name
   priority                    = 100
@@ -53,8 +69,11 @@ resource "azurerm_network_security_rule" "http_rule" {
   destination_address_prefix  = "*"
   network_security_group_name = azurerm_network_security_group.nsg.name
   resource_group_name         = azurerm_resource_group.rg.name
+
+  depends_on = [azurerm_network_security_group.nsg]
 }
 
+# Create the SSH rule for the NSG
 resource "azurerm_network_security_rule" "ssh_rule" {
   name                        = var.ssh_rule_name
   priority                    = 101
@@ -67,8 +86,11 @@ resource "azurerm_network_security_rule" "ssh_rule" {
   destination_address_prefix  = "*"
   network_security_group_name = azurerm_network_security_group.nsg.name
   resource_group_name         = azurerm_resource_group.rg.name
+
+  depends_on = [azurerm_network_security_group.nsg]
 }
 
+# Create the network interface
 resource "azurerm_network_interface" "nic" {
   name                = var.network_interface_name
   location            = var.location
@@ -83,18 +105,22 @@ resource "azurerm_network_interface" "nic" {
 
   tags = var.tags
 
-  # Explicit dependency added
-  depends_on = [azurerm_resource_group.rg, azurerm_public_ip.pip]
+  depends_on = [
+    azurerm_subnet.subnet,
+    azurerm_public_ip.pip,
+    azurerm_network_security_group.nsg
+  ]
 }
 
+# Associate the NIC with the NSG
 resource "azurerm_network_interface_security_group_association" "nsg_association" {
   network_interface_id      = azurerm_network_interface.nic.id
   network_security_group_id = azurerm_network_security_group.nsg.id
 
-  # Explicit dependency added
-  depends_on = [azurerm_network_interface.nic, azurerm_network_security_group.nsg]
+  depends_on = [azurerm_network_interface.nic]
 }
 
+# Create Linux VM with password authentication
 resource "azurerm_linux_virtual_machine" "vm" {
   name                            = var.vm_name
   location                        = var.location
@@ -137,6 +163,5 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   tags = var.tags
 
-  # Explicit dependency added
-  depends_on = [azurerm_network_interface.nic, azurerm_public_ip.pip]
+  depends_on = [azurerm_network_interface.nic]
 }
--- task04/outputs.tf
+++ task04/outputs.tf
@@ -1,3 +1,8 @@
+output "resource_group_id" {
+  value       = azurerm_resource_group.rg.id
+  description = "The ID of the resource group created."
+}
+
 output "vm_public_ip" {
   value       = azurerm_public_ip.pip.ip_address
   description = "The public IP address of the virtual machine."
